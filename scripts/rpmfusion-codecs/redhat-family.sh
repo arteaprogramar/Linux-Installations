17f123c5304e3ab0aaf1d7a45910ca23fe407e77
Arte a Programar <arte.aprogramar@gmail.com>
2021-12-31 05:45:25 -0600

    RPMFusion for Centos Stream 9

--- scripts/rpmfusion-codecs/redhat-family.sh
+++ scripts/rpmfusion-codecs/redhat-family.sh
@@ -5,8 +5,13 @@ logger(){
 }
 
 osName=$(((egrep '^(NAME)=' /etc/os-release) | awk -F = {'print $2'}) | tr -d \")
-fedoraRelease=$(cat /etc/os-release | grep "VERSION_ID=")
-fedoraId=$(rpm -E %fedora)
+
+if [[ $osName = "Fedora Linux" ]]; then
+    osNumber=$(rpm -E %fedora)
+elif [[ $osName = "CentOS Stream" ]]; then
+    osNumber=$(rpm -E %centos)
+fi
+
 
 # Limpiar consola
 echo $(clear)
@@ -14,17 +19,17 @@ echo $(clear)
 # Mostrar información del Script
 logger "Arte a Programar"
 logger "Script para instalar RPM Fusion free/nonfree y Codecs"
-logger "Disponible solo para Fedora"
 logger "v0.1\n"
-logger "OS : " $osName $fedoraId
+logger "OS : " $osName $osNumber
+
+
+# Realizar actualización del Sistema
+logger "Para ejecutar este script se requiere acceso administrativo"
+logger "Realizando actualización del sistema"
+sudo dnf -y update
 
 # Comprobación de OS
 if [[ $osName = "Fedora Linux" ]]; then
-    # Realizar actualización del Sistema
-    logger "Para ejecutar este script se requiere acceso administrativo"
-    logger "Realizando actualización del sistema"
-    sudo dnf -y update
-
     # Instalación de Repositorio RPM Fusion Free
     logger "\n\nConsulte la documentación oficial"
     logger "https://docs.fedoraproject.org/en-US/quick-docs/setup_rpmfusion/"
@@ -46,7 +51,21 @@ if [[ $osName = "Fedora Linux" ]]; then
     sudo dnf -y install vlc
 
     # Finalizar
-    logger "\n\nSe instalado Codecs de Música, Video y Repositorio RPMFusion"        
+    logger "\n\nSe ha instalado Codecs de Música, Video y Repositorio RPMFusion"        
+elif [[ $osName = "CentOS Stream" ]]; then
+    if [[ $osNumber = "9" ]]; then 
+        # Nota:  CentOS Stream 9
+        logger "\n\nActualmente no existe soporte de RPMFusion para CentOS Stream 9"
+
+        # Installing plugins for playing movies and music
+        logger "\n\nInstalación de complementos para reproducir películas y música"
+        sudo dnf -y install gstreamer1-plugins-{bad-\*,good-\*,base}  --exclude=gstreamer1-plugins-bad-free-devel
+        sudo dnf -y install lame\* --exclude=lame-devel
+        sudo dnf -y group upgrade --with-optional Multimedia
+
+        # Finalizar
+        logger "\n\nSe ha instalado Codecs de Música y Video"      
+    fi
 else
     logger "Tu sistema operativo no es compatible por ahora"  
 fi
