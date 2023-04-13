b0d838b000dbb62ec93361fadaf1ce586664c4b0
arteaprogramar <arte.aprogramar@gmail.com>
2023-04-13 16:59:10 -0600

    Script para instalación de Java Open

--- src/app/pacman/java.py
+++ src/app/pacman/java.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 
+import main
 from src.config import Printing
 
 _TITLE = 'Instalación de Java (JRE - JDK)'
@@ -13,13 +14,32 @@ def init(pkg: str):
     Printing.title('Buscar versiones disponibles de Java open-(jre/jdk)', False)
     pacman_search = subprocess.check_output("pacman -Ss jre | grep -E 'jre([0-9]{0,2})-openjdk '", shell=True, text=True)
     pacman_search = pacman_search.split("\n")
-    versions = filter(lambda p: p != '', pacman_search)
+    versions = list(filter(lambda pkg: pkg != '', pacman_search))
+    versions = list(map(lambda pkg: pkg[(pkg.find('/') + 1): pkg.find(' ')], versions))
 
     Printing.title('Lista de versiones disponibles', False)
     selected = -1
     continue_menu = True
 
     for index, version in enumerate(versions):
-        print([version.count('/')])
-        version = version[version.count('/'):]
         Printing.message(f"Para instalar Java \033[1m{version}\033[0m ingrese \033[1m{index}\033[0m")
+
+    while continue_menu:
+        selected = main.parser_int(input('¿Que version de Java deseas instalar? : '))
+
+        if selected < len(pacman_search):
+            continue_menu = False
+
+    Printing.title('Instalación de Java')
+    text_version = versions[selected]
+
+    version = main.parser_int(''.join(filter(str.isdigit, text_version)))
+
+    if version == -1:
+        version = ''
+
+    jre = f'jre{version}-openjdk'
+    jdk = f'jdk{version}'
+
+    packages = f'{jre}-headless jre{version}-openjdk {jdk}-openjdk openjdk{version}-doc openjdk{version}-src'
+    os.system(f'sudo pacman -S {packages} --noconfirm')
