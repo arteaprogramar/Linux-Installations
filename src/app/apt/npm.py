f02eb421150552da929701f064a2e0008dc73713
Arte a Programar <65364287+arteaprogramar@users.noreply.github.com>
2024-06-25 11:36:31 -0600

    Create npm.py

---
+++ src/app/apt/npm.py
@@ -0,0 +1,21 @@
+import os
+import platform
+
+from src.config import Printing
+
+_TITLE = 'Instalación de Node 20+'
+
+
+def init(manager: str):
+    Printing.welcome(_TITLE)
+    Printing.message('')
+
+    Printing.title('Actualizar Sistema', True)
+
+    Printing.title('Instalación de NodeJS')
+    os.system(f'sudo curl -sL https://deb.nodesource.com/setup_20.x -o /tmp/nodesource_setup.sh')
+    os.system(f'sudo bash /tmp/nodesource_setup.sh')
+    os.system(f'sudo apt install -y nodejs')
+    os.system(f'node --version')
+
+
