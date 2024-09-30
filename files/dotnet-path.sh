72f63324949a20228c443e82d24b76cc5611fc3f
arteaprogramar <arteaprogramar@gmail.com>
2024-09-29 22:54:47 -0600

    Experimental v0.1.8-exp | Instalación de .NET

---
+++ files/dotnet-path.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Archivo de configuracion para exportar al Path Linux
+export DOTNET_HOME=/usr/share/dotnet
+export PATH=${DOTNET_HOME}:${PATH}
---
+++ files/dotnet-repository.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+versions=("6" "7" "8")
+
+if [ -z "$1" ]; then
+    echo "Descargar .NET Script"
+
+    wget https://dot.net/v1/dotnet-install.sh -P temp
+    chmod +x temp/dotnet-install.sh
+
+    printf '%s\n' "${versions[@]}"
+    exit 0
+fi
+
+dotnet_version=$1
+
+for version in "${versions[@]}"; do
+    if [ "$dotnet_version" = "$version" ]; then
+        echo "Se instalará .NET $dotnet_version"
+        sudo bash temp/dotnet-install.sh --runtime dotnet --version "$dotnet_version".0.0 --install-dir /usr/share/dotnet
+        sudo bash temp/dotnet-install.sh --channel "$dotnet_version".0.1xx --install-dir /usr/share/dotnet
+        exit 0
+    fi
+done
+
+echo "Se ha seleccionado una opción invalida"
+exit 1
