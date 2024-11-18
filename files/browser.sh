150aadfeef8afa529dba3cdd46b955d055810a4f
arteaprogramar <arteaprogramar@gmail.com>
2024-11-18 13:09:15 -0600

    Experimental v1.0.1 | Correcciíon de error al instalar Chrome & Edge

--- files/browser.sh
+++ files/browser.sh
@@ -4,19 +4,31 @@ manager=$1
 
 if "$manager" --version > /dev/null 2>&1; then
 
-    if [ "$manager" == "dnf" ]; then
+    if [ "$manager" = "dnf" ]; then
         sudo dnf -y install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-        sudo dnf -y install https://packages.microsoft.com/yumrepos/edge/microsoft-edge-stable-129.0.2792.65-1.x86_64.rpm
+
+        # Instalación de Microsoft Edge
+        sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+        sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
+        sudo mv /etc/yum.repos.d/packages.microsoft.com_yumrepos_edge.repo /etc/yum.repos.d/microsoft-edge.repo
+        sudo dnf install microsoft-edge-stable
     fi
 
-    if [ "$manager" == "apt" ]; then
-        wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_129.0.2792.65-1_amd64.deb  -P temp
+    if [ "$manager" = "apt" ]; then
         wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P temp
-        sudo apt -y install temp/google-chrome-stable_current_amd64.deb
-        sudo apt -y install temp/microsoft-edge-stable_129.0.2792.65-1_amd64.deb
+        sudo dpkg -i temp/google-chrome-stable_current_amd64.deb
+        sudo apt -f install -y
+
+         # Instalación de Microsoft Edge
+        curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+        sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/
+        sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-stable.list'
+        sudo rm microsoft.gpg
+        sudo apt update
+        sudo apt install microsoft-edge-stable
     fi
 
-    if [ "$manager" == "yay" ]; then
+    if [ "$manager" = "yay" ]; then
         yay -S google-chrome --noconfirm;
         yay -S microsoft-edge-stable-bin --noconfirm
     fi
--- files/php-repository.sh
+++ files/php-repository.sh
@@ -2,11 +2,11 @@
 
 os_id=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
 
-if [[ $os_id == "debian" || $os_id == "ubuntu" ]]; then
+if [[ $os_id = "debian" || $os_id = "ubuntu" ]]; then
     echo "Estas ejecutando GNU Linux : $os_id"
 
     # Instrucciones para Debian
-    if [ $os_id == "debian" ]; then
+    if [ $os_id = "debian" ]; then
 
         sudo apt install lsb-release apt-transport-https ca-certificates  wget -y
         sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
@@ -15,7 +15,7 @@ if [[ $os_id == "debian" || $os_id == "ubuntu" ]]; then
         codename=$(lsb_release -sc)
 
         # Validar si se usa Debian Testing con nombre base "trixie"
-        if [[ $codename == "trixie" || $codename == "testing" ]]; then
+        if [[ $codename = "trixie" || $codename = "testing" ]]; then
             echo "deb https://packages.sury.org/php/ bookworm main" | sudo tee /etc/apt/sources.list.d/php.list
         else
             echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
